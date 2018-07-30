a025d7e2c159502f284ec1e205e46b9c187570a4
Karl Knight <40666322+VR40Disc@users.noreply.github.com>
2018-07-30 12:49:39 +0100

    Create T-SQL-DECLARE-LOCAL-VARIABLE.sql

---
+++ 70-761/Manage-data/Samples/T-SQL-DECLARE-LOCAL-VARIABLE.sql
@@ -0,0 +1,72 @@
+--
+-- https://docs.microsoft.com/en-us/sql/t-sql/language-elements/declare-local-variable-transact-sql?view=sql-server-2017
+--
+-- DECLARE @local_variable
+-- 
+-- Variables are declared in the body of a batch or procedure with the DECLARE statement.
+--
+-- Cursor variables can be declared with this statement and used with other cursor related statements.
+--
+-- After declaration, all variables are initialized as NULL, unless a value is provided as part of the declaration.
+--
+-- Syntax:
+--
+-- DECLARE   
+--   {   
+--       { @local_variable [AS] data_type  [ = value ] }  
+--     | { @cursor_variable_name CURSOR }  
+--   } [,...n]   
+--   | { @table_variable_name [AS] <table_type_definition> }   
+--   
+--   <table_type_definition> ::=   
+--        TABLE ( { <column_definition> | <table_constraint> } [ ,...n] )   
+--   
+--   <column_definition> ::=   
+--        column_name { scalar_data_type | AS computed_column_expression }  
+--        [ COLLATE collation_name ]   
+--        [ [ DEFAULT constant_expression ] | IDENTITY [ (seed ,increment ) ] ]   
+--        [ ROWGUIDCOL ]   
+--        [ <column_constraint> ]   
+--   
+--   <column_constraint> ::=   
+--        { [ NULL | NOT NULL ]   
+--        | [ PRIMARY KEY | UNIQUE ]   
+--        | CHECK ( logical_expression )   
+--        | WITH ( <index_option > )  
+--        }   
+--   
+--   <table_constraint> ::=   
+--        { { PRIMARY KEY | UNIQUE } ( column_name [ ,...n] )   
+--        | CHECK ( search_condition )   
+--        }   
+--   
+--   <index_option> ::=  
+--   See CREATE TABLE for index option syntax.
+--
+
+-- Declare a variable initialized to NULL then set it's value
+DECLARE @colour VARCHAR(50);
+SET @colour = 'RED';
+SELECT COUNT(*) AS 'No. of Red Products'
+  FROM dbo.MySimpleProducts
+ WHERE p_colour = @colour;
+GO
+
+-- Declaring multiple variables initialized to NULL then setting their values
+DECLARE @red VARCHAR(50), @blue VARCHAR(50);
+SET @red = 'RED';
+SET @blue = 'BLUE';
+SELECT p_colour AS 'Color', COUNT(*) AS 'No. of Products'
+  FROM dbo.MySimpleProducts
+ WHERE p_colour IN (@red, @blue)
+ GROUP BY p_colour;
+GO
+
+-- Declare and initialization done in the DECLARE statment (example as above)
+DECLARE @red VARCHAR(50) = 'RED', 
+        @blue VARCHAR(50) = 'BLUE';
+SELECT p_colour AS 'Color', COUNT(*) AS 'No. of Products'
+  FROM dbo.MySimpleProducts
+ WHERE p_colour IN (@red, @blue)
+ GROUP BY p_colour;
+GO
